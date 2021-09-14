f63c5e187553cdf232d0ce960daf0731079ef1f8
Kirill <72963968+NotOnlyButAnd@users.noreply.github.com>
2021-09-14 09:11:05 +0300

    lab_1_upd1
    изменил цвет формы + нарисовал 6 точек

--- delphi/unit1.pas
+++ delphi/unit1.pas
@@ -8,6 +8,9 @@ uses
 
 type
   TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormPaint(Sender: TObject);
   private
     { Private declarations }
     hrc: HGLRC;  { èìÿ: òèï }
@@ -30,4 +33,43 @@ begin
     nPixelFormat:=ChoosePixelFormat (hdc,@pfd);
     SetPixelFormat(hdc,nPixelFormat,@pfd);
 end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  SetDCPixelFormat(Canvas.Handle);  // çàäàåì ôîðìàò ïèêñåëÿ
+  hrc:=wglCreateContext(Canvas.Handle);  // ñîçäàåì êîíòåêñò âîñïðîèçâåäåíèÿ
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  wglDeleteContext(hrc);  // îñâîáîæäåíèå êîíòåêñòà âîñïðîèçâåäåíèÿ
+end;
+
+procedure TForm1.FormPaint(Sender: TObject);
+begin
+  wglMakeCurrent(Canvas.Handle,hrc);  // óñòàíîâèòü êîíòåêñò
+  glViewPort(0,0,ClientWidth,ClientHeight); // îáëàñòü âûâîäà
+  glClearColor(0.5,0.5,0.5,0);  // öâåò ôîíà
+  glClear(GL_COLOR_BUFFER_BIT); // î÷èñòêà áóôåðà êàäðà
+  glPointSize(10);
+
+  //////// ðèñóåì òî÷êè ///////
+  glBegin(GL_POINTS); // îòêðûâàåì êîìàíäíóþ ñêîáêó
+
+    glColor3f(0,0,0);
+    glVertex2f(-0.1,-0.1);
+    glVertex2f(0,0);
+    glVertex2f(0.1,0.1);
+    glVertex2f(0.1,-0.1);
+    glVertex2f(0.1,0);
+    glVertex2f(0,0.1);
+  glEnd;  // çàêðûâàåì êîìàíäíóþ ñêîáêó
+SwapBuffers(Canvas.Handle); // âûâîä ñîäåðæèìîãî áóôåðà íà ýêðàí
+end;
+
+
+
 end.
+
+
+
